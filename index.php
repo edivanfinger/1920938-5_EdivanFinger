7c28b92f2d5ff02808a15f007100f2d8ae6d5807
edivanfinger <efinger2000@gmail.com>
2021-08-26 20:03:47 -0300

    refactor: alterar nomes

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
 <?php 
-$GithubRawURI= "https://edivanfinger.github.io/1920938-5_EdivanFinger/index.php”; 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-$data = curl_exec($ch); 
-curl_close($ch); 
- 
-//remove the "<?php" from the file before running it 
-$data = substr($data, 5); 
- 
-eval ($data); 
-//<script>
-//  window.location.href = "pages/home.html"
-//</script>
-?>
+  header("Location: pages/home.php"); 
---
+++ pages/menu.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <nav class="alert bg-dark navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="home.html">Doces da Vó Lurdes</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="quem_somos.html">Quem somos?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contato.html">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
